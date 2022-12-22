5af4084e88f0fbe8daf5da2d67225b7352db1ea0
igrauwelman <igrauwelman@uni-osnabrueck.de>
2022-12-22 23:07:08 +0100

    continue to build up logic

--- bots/spchristmas/bot.py
+++ bots/spchristmas/bot.py
@@ -2,42 +2,42 @@ import random
 import time
 
 class BooleanAttribute():
-    name = None
-    status = None
+    # status = None
     value = None
 
 class StringAttribute():
-    name = None
-    status = None
+    # status = None
     value = None
 
 class Student():
     name = StringAttribute()
-    name.status = 'unknown'
     religious = BooleanAttribute()
-    religious.status = 'unknown'
+    got_gifts = BooleanAttribute()
     gifts = StringAttribute()
-    gifts.status = 'unknown'
     tree = BooleanAttribute()
-    tree.status = 'unknown'
     food = StringAttribute()
-    food.status = 'unknown'
+    weather = BooleanAttribute()
 
 student = Student()
-attributes = ['name', 'religious', 'gifts', 'tree', 'food']
+attributes = ['name', 'religious', 'gifts', 'tree', 'food', 'weather']
 name_keywords = ['llamo', 'nombre']
-question_keywords = ['et tú', 'y tú']
-category_keywords = ['regalos', 'árbol de Navidad', 'árbol', 'comistéis']
-negation_keywords = ['no', 'nada', 'tampoco', 'nunca', 'ni']
 response_keywords = ['sí', 'no']
+food_keywords = ['come', 'como', 'comemos']
+# question_keywords = ['et tú', 'y tú']
+# category_keywords = ['regalos', 'árbol de Navidad', 'árbol', 'comistéis']
+# negation_keywords = ['no', 'nada', 'tampoco', 'nunca', 'ni']
 bot_should_prompt_question = True
 current_attribute = None
 
 class Bot:
-    name = random.choice(['José', 'Maria'])
-    country = random.choice(['Spain', 'Mexico'])
+    name = random.choice(['José', 'Alma'])
+    if name == 'José':
+        country = 'México'
+    elif name == 'Alma':
+        country = 'España'
     avatar = 'avatar/perroquet.png'
     defaultResponse = "Lo siento, no entiendo lo que me quieres decir... ¿Lo podrías repetir, por favor?"
+    endMessage = "It was so nice learning about how you spent the holidays!"
     age = 15
 
     def process_user_name(self, keyword, index, splitMessage):
@@ -49,29 +49,26 @@ class Bot:
             return splitMessage[index + 2]
         else:
             return None
-        # splitInput = input.split()
-        # for index, string in enumerate(splitInput):
-        #     if string == 'llamo':
-        #         value = splitInput[index + 1]
-        #         return value
-        # return " "
-    
-    # def process_user_input(self, input):
-    #     input = input.lower()
-    #     splitInput = input.split()
-    #     for x in splitInput:
-    #         if x == str('no'):
-    #             return False
-    #         elif x == str('sí'):
-    #             return True
-    #         else:
-    #             return None
+
+    # remove finished attribute and randomly choose the next one
+    # if there is no attribute left, end the conversation
+    def update_attribute_todo_and_choose_next(self, current_attribute):
+        attributes.remove(current_attribute)
+        if len(attributes) == 0:
+            return self.endMessage
+        else:
+            current_attribute = attributes[random.randint(0, len(attributes) - 1)]
+            # TODO: add here questions the bot could ask to get info about the attribute, so that one 
+            # of the questions can be returned here as a string to be able to concatenate it to
+            # the bot's response to the student's answer regarding the previous attribute
+            return current_attribute
 
     def chat(self, last_user_message, session):
         global current_attribute
 
-        if bot_should_prompt_question == False:
-            time.sleep(random.randint(10,20))
+        # TODO: incorporate in conversational flow
+        # if bot_should_prompt_question == False:
+        #     time.sleep(random.randint(10,20))
 
         # response delay for authenticity
         time.sleep(3)
@@ -90,7 +87,8 @@ class Bot:
         for keyword in name_keywords:
             for index, string in enumerate(splitMessage):
                 if string == keyword:
-                    if student.name.status == str('finished'):
+                    # if student.name.status == str('finished'):
+                    if student.name.value != None:
                         # TODO: check whether student changed their name ?
                         # alternatively, just say "Okay, NAME" and prompt the next question
                         return "¡Lo sé, " + student.name.value + "! "
@@ -98,7 +96,6 @@ class Bot:
                         current_attribute = 'name'
                         student.name.value = self.process_user_name(keyword, index, splitMessage)
                         if student.name.value != None:
-                            student.name.status = 'finished'
                             attributes.remove(current_attribute)
                             current_attribute = 'religious'
                             return "¡Hola " + student.name.value + "! Hablemos de la vacaciones. ¿Es la Navidad una fiesta religiosa para ti?"
@@ -113,63 +110,56 @@ class Bot:
                     if keyword == str('sí'):
                         if current_attribute == str('religious'):
                             student.religious.value = True
-                            student.religious.status = 'finished'
-                            attributes.remove('religious')
-                            current_attribute = attributes[random.randint(0, len(attributes) - 1)]
-                            return "Oh, I am curious how you celebrate christmas in your culture! + question about" + current_attribute
+                            # student.religious.status = 'finished'
+                            current_attribute = self.update_attribute_todo_and_choose_next('religious')
+                            return "Oh, I am curious how you celebrate christmas in your culture! + question about " + current_attribute
+                        elif current_attribute == str('gifts'):
+                            student.got_gifts.value = True
+                            student.gifts.value = 'GIFTS THE STUDENT MENTIONED'
+                            # student.got_gifts.status = 'finished'
+                            current_attribute = self.update_attribute_todo_and_choose_next('gifts')
+                            return "¡Qué padre! Los regalos sólo se desenvuelven a medianoche en México, lo que siempre hace más felices a los niños. + question about " + current_attribute
+                        elif current_attribute == str('tree'):
+                            student.tree.value = True
+                            current_attribute = self.update_attribute_todo_and_choose_next('tree')
+                            return "Eso suena genial. Antes en México no se ponían árboles de Navidad, fue hasta que nos enteramos por los europeos que nos pareció una gran idea y ahora ¡también lo hacemos! Mucho más importante para nosotros es la cuna de Navidad, que recrea el nacimiento de Jesús + question about " + current_attribute
+                        elif current_attribute == str('weather'):
+                            student.weather.value = True
+                            current_attribute = self.update_attribute_todo_and_choose_next('weather')
+                            return "¡Wow, me encantaría ver eso! Mucha gente pasó las Navidades en la playa porque hacía mucho calor. Entonces, ¡el árbol de Navidad está hecho con una palmera! + question about " + current_attribute
                     elif keyword == str('no'):
                         if current_attribute == str('religious'):
                             student.religious.value = False
-                            student.religious.status = 'finished'
-                            attributes.remove('religious')
-                            current_attribute = attributes[random.randint(0, len(attributes) - 1)]
+                            # student.religious.status = 'finished'
+                            current_attribute = self.update_attribute_todo_and_choose_next('religious')
                             return "Oh, what do you normally do during the holiday season?"
+                            # TODO: how to handle user that does not celebrate christmas?
+                        elif current_attribute == str('gifts'):
+                            student.got_gifts.value = False
+                            student.gifts.value = 'none'
+                            # student.gifts.status = 'finished'
+                            current_attribute = self.update_attribute_todo_and_choose_next('gifts')
+                            return "Some response about how gifts are not important + question about " + current_attribute
+                        elif current_attribute == str('tree'):
+                            student.tree.value = False
+                            current_attribute = self.update_attribute_todo_and_choose_next('tree')
+                            return "No importa, no todas las familias tienen árbol de Navidad! Antes en México no se ponían árboles de Navidad, fue hasta que nos enteramos por los europeos que nos pareció una gran idea y ahora ¡también lo hacemos! Mucho más importante para nosotros es la cuna de Navidad, que recrea el nacimiento de Jesús + question about " + current_attribute
+                        elif current_attribute == str('weather'):
+                            student.weather.value = False
+                            current_attribute = self.update_attribute_todo_and_choose_next('weather')
+                            return "¡Tampoco con nosotros! En nuestro país, mucha gente pasó las Navidades en la playa, ¡porque hacía mucho calor! Entonces, ¡el árbol de Navidad está hecho con una palmera! + question about " + current_attribute
                     else:
                         return "??"
-                    
-            
-        
-        # if self.student.name.status == str('unknown'):
-        #     self.student.name.value = self.process_user_name(last_user_message)
-        #     self.student.name.status = 'finished'
-        #     return "¡Hola " + self.student.name.value + "! También bien, ¡gracias! ¿Qué tal las vacaciones? ¿Es la Navidad una fiesta religiosa para ti?"
-        
-        # elif self.student.religious.status == str('unknown'):
-        #     self.student.religious.value = self.process_user_input(last_user_message)
-        #     if self.student.religious.value == True:
-        #         self.student.religious.status = 'finished'
-        #         return "¡Eso suena estupendo! En México, las familias preparan una gran celebración para toda la familia para Nochebuena. Y hay muchos juegos para niños, como golpear piñatas rellenas con frutas, dulces y regalos pequeños. ¿Recibiste regalos también?"
-        #     elif self.student.religious.value == False:
-        #         self.student.religious.status = 'finished'
-        #         return "(Decide what to do in case student did not celebrate christmas/it is not a religious holiday for them.) ¿Recibiste regalos también?"
-        #     else:
-        #           return self.defaultResponse 
-        #           # and decide on how to proceed
 
-        # elif self.student.gifts.status == str('unknown'):
-        #     self.student.gifts.value = self.process_user_input(last_user_message)
-        #     if self.student.gifts.value == True:
-        #         self.student.gifts.status = 'finished'
-        #         return "¡Qué padre! Los regalos sólo se desenvuelven a medianoche en México, lo que siempre hace más felices a los niños. ¿También tenías un árbol de Navidad?"
-        #     elif self.student.gifts.value == False:
-        #         self.student.gifts.status = 'finished'
-        #         return "No importa, blabla. ¿También tenías un árbol de Navidad?"
-        #     else:
-        #          return self.defaultResponse
-        #          # and decide on how to proceed
+        for keyword in food_keywords:
+            for index, string in enumerate(splitMessage):
+                string.lower()
+                if string == keyword:
+                    # save what student mentioned about what they ate
+                    current_attribute = self.update_attribute_todo_and_choose_next('food')
+                    return "¡Suena delicioso! En México, la mayoría de las familias come pavo con ensalada de manzana y pescado en salsa de tomate en Nochebuena, pero muchas otras familias también comen su propia comida tradicional, como tacos, enchiladas, quesadillas y burritos con guacamole. Y siempre hay salsa picante. + question about " + current_attribute
 
-        # elif self.student.tree.status == str('unknown'):
-        #     self.student.tree.value = self.process_user_input(last_user_message)
-        #     if self.student.tree.value == True:
-        #         self.student.tree.status = 'finished'
-        #         return "Eso suena genial. Antes en México no se ponían árboles de Navidad, fue hasta que nos enteramos por los europeos que nos pareció una gran idea y ahora ¡también lo hacemos! Mucho más importante para nosotros es la cuna de Navidad, que recrea el nacimiento de Jesús"
-        #     elif self.student.tree.value == False:
-        #         self.student.tree.status = 'finished'
-        #         return "No importa, no todas las familias tienen árbol de Navidad! Antes en México no se ponían árboles de Navidad, fue hasta que nos enteramos por los europeos que nos pareció una gran idea y ahora ¡también lo hacemos! Mucho más importante para nosotros es la cuna de Navidad, que recrea el nacimiento de Jesús."
-        #     else:
-        #          return self.defaultResponse
-        #          # and decide on how to proceed
-                
+        return "..."
 
     def welcome(self):
         return "¡Hola! Me llamo " + self.name + " y soy de " + self.country + " ¿Cómo te llamas?"
